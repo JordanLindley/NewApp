e0b2a7c395426a1b3f5748c6045803d6c2650d14
Scott Lindley <scottdlindley@gmail.com>
2018-05-12 15:13:19 -0400

    text field and send button

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    message: 'Hey there',
+    messageField: 'Write your message here',
+  };
+
+  clickSend = () => {
+    this.setState(previousState => ({ message: previousState.messageField }));
+  };
+
+  onChangeText = text => {
+    this.setState({ messageField: text })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={ styles.container }>
+        <Text style={ styles.text }>{ this.state.message }</Text>
+        <TextInput
+          style={ styles.input }
+          onChangeText={ this.onChangeText }>
+          { this.state.messageField }
+        </TextInput>
+        <Button
+          style={ styles.button }
+          title="Send"
+          color="#EF7177"
+          onPress={ this.clickSend }/>
       </View>
     );
   }
@@ -18,6 +39,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: '20%'
   },
+  button: {
+    height: '80%',
+    width: '80%'
+  },
+  text: {
+    fontSize: 42,
+    marginBottom: 20
+  },
+  input: {
+    fontSize: 28,
+    marginBottom: 20
+  }
 });
